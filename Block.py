5e03a83ac0b6fa840b785b02dc62c7c308a0ce6f
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2023-06-08 02:20:15 +0200

    Develop (#3)
    * - redo the Block constructor, rename shape to rect
    
    * Squashed commit of the following:
    
    commit e5fe483d2b7c14422fb7ab334cee8f6943cce98c
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:01:36 2023 +0200
    
        - redo main base on changes
    
    commit f8f041afc7c17082d6033783a37c1d8adc214983
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:00:53 2023 +0200
    
        - added colors of block base on option
    
    commit e06405d82627383d01132db91190156a2d2c4adf
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:00:16 2023 +0200
    
        - changed map and added to consts, added gray

--- Block.py
+++ Block.py
@@ -6,17 +6,20 @@ pygame.font.init()
 
 class Block:
     count = 0  # Class variable to keep track of the block count
-    map = {"testcasegroup": 0,"testcase": 1,"parameter": 2,}
     font = pygame.font.Font('freesansbold.ttf', 32)
-    def __init__(self, name: str, shape: pygame.Rect, text):
+
+    def __init__(self, name: str, position_and_size: tuple[int,int,int,int], text):
         self.name = name
         self.number = Block.count + 1
         self.id = f"{self.name}_{self.number}"
         self.hover = False
-        self.shape = shape
-        self.text =  Block.font.render(text, True, BLUE, RED_LIGHT)
+        self.rect = pygame.Rect(position_and_size)
+        print(MAP_COLOR[self.name])
+        self.color = MAP_COLOR[self.name] or WHITE
+        self.dimmed_color = tuple(int(component * DIM_FACTOR) for component in self.color)
+        self.text =  Block.font.render(text, True, BLACK, self.dimmed_color)
         self.text_rect  = self.text.get_rect()
-        self.text_rect.center = self.shape.center
+        self.text_rect.center = self.rect.center
         self.params = {}
         Block.count += 1
         self.children = []
@@ -28,11 +31,11 @@ class Block:
         self.children.append(child_block)
     
     def update_position(self,position: tuple[int,int]):
-        self.shape.center = position
-        self.text_rect.center = self.shape.center
+        self.rect.center = position
+        self.text_rect.center = self.rect.center
 
     def draw_on(self,screen: pygame.Surface):
-        pygame.draw.rect(screen,self.params["color"] or RED, self.shape)
+        pygame.draw.rect(screen,self.color, self.rect)
         screen.blit(self.text,self.text_rect)
         for child in self.children:
             child.draw(screen)
@@ -42,12 +45,12 @@ class Block:
 
     def __eq__(self, other):
         if isinstance(other, Block):
-            return self.map[self.name] == other.map[self.name]
+            return MAP_NAME[self.name] == MAP_NAME[other.name]
         return NotImplemented
 
     def __lt__(self, other):
         if isinstance(other, Block):
-            return self.map[self.name] < other.map[self.name]
+            return MAP_NAME[self.name] < MAP_NAME[other.name]
         return NotImplemented
     
     def __str__(self) -> str:
