c15bd8d80e016086118e0e37e105387889811d8e
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2023-06-08 02:02:54 +0200

    Squashed commit of the following:
    commit e5fe483d2b7c14422fb7ab334cee8f6943cce98c
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:01:36 2023 +0200
    
        - redo main base on changes
    
    commit f8f041afc7c17082d6033783a37c1d8adc214983
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:00:53 2023 +0200
    
        - added colors of block base on option
    
    commit e06405d82627383d01132db91190156a2d2c4adf
    Author: Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
    Date:   Thu Jun 8 02:00:16 2023 +0200
    
        - changed map and added to consts, added gray

--- consts.py
+++ consts.py
@@ -1,20 +1,21 @@
 # Colors
 BLACK       = (0, 0, 0)
+GRAY        = (128, 128, 128)
 WHITE       = (255, 255, 255)
 RED         = (255, 0, 0)
 RED_LIGHT   = (255, 77, 77)
 BLUE        = (102, 204, 255)
 GREEN       = (46, 184, 46)
 YELLOW      = (255, 204, 102)
-
+DIM_FACTOR = 0.85
 # Set up the window dimensions
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH    = 800
+WINDOW_HEIGHT   = 600
 
 # Button parameters
-BUTTON_WIDTH = 100
-BUTTON_HEIGHT = 30
-BUTTON_COLOR = (100, 100, 100)
+BUTTON_WIDTH    = 100
+BUTTON_HEIGHT   = 30
+BUTTON_COLOR    = (100, 100, 100)
 BUTTON_TEXT_COLOR = WHITE
 
 # Options for tk window
@@ -24,4 +25,14 @@ OPTIONS = [
     "parameter",
     ] #etc
 
+MAP_NAME = {
+    "testcasegroup" : 0,
+    "testcase"      : 1,
+    "parameter"     : 2,
+}
+MAP_COLOR = {
+    "testcasegroup" : YELLOW,
+    "testcase"      : RED_LIGHT,
+    "parameter"     : BLUE,
+}
 MOUSE_POS = []
