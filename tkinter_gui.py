f786fa7fe580cbecf4a3dc78af6b4fdcb0fae7d9
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2023-06-25 14:02:19 +0200

    - changed widgets list to dictionary - added update_block function

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -34,26 +34,24 @@ class TkinterGui:
         self.footer.grid_columnconfigure(2, weight=1)
         self.footer.grid_columnconfigure(3, weight=1)
         self.footer.grid_columnconfigure(4, weight=2)
-        self.widgets: List[tuple[tk.Label,ttk.Entry]] = []
+        self.widgets: Dict[str,tuple[tk.Label,ttk.Entry]] = {}
 
-        self.block_id = tk.Label(self.box,text="Block ID:",relief="groove")
-        self.block_id.grid(column=0,columnspan=2,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
+        self.block_id = tk.Label(self.box,text="Block ID: ",relief="groove")
+        self.block_id.grid(column=0,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
+        self.block_id_value = tk.Label(self.box,text="None",relief="groove")
+        self.block_id_value.grid(column=1,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
         TkinterGui.label_row_count+=1
 
         for parameter in BLOCK_PARAMETERS:
-            self.widgets.append( self.set_lab_and_enter(f"{parameter}:",TkinterGui.label_row_count) )
-
-        for idx,widget in enumerate(self.widgets):
-            widget[1].bind('<Return>', lambda event, nr=idx: self.handle_enter(event, nr))
+            self.widgets[parameter] = self.set_lab_and_enter(f"{parameter}:",parameter,TkinterGui.label_row_count)
+            self.widgets[parameter][1].bind('<Return>', lambda event, name=parameter: self.handle_enter(event, name))       
 
         self.button1 = self.set_button(parent=self.footer,option=0,_row=0,_col=0,_padx=(5,0),_pady=(5,5),_sticky="nsew")
         self.button2 = self.set_button(parent=self.footer,option=1,_row=0,_col=1,_padx=(5,0),_pady=(5,5),_sticky="nsew")
         self.button3 = self.set_button(parent=self.footer,option=2,_row=0,_col=2,_padx=(5,0),_pady=(5,5),_sticky="nsew")
-
+        
         self.save_button = tk.Button(self.footer, text="Save", command=lambda: self.window.save_xml())
         self.save_button.grid(row=0,column=4,padx=(0,5),pady=(5,5),sticky="nsew")
-
-        self.button1.configure(activebackground="black")
     
     def run_gui(self):
         self.root.after(100, self.process_message_queue)
@@ -76,27 +74,30 @@ class TkinterGui:
         TkinterGui.label_row_count+=1
         return label
     
-    def set_entery(self,_row=0,_col=0):
-        entry = ttk.Entry(self.box)
+    def set_entery(self,_name="none",_row=0,_col=0):
+        entry = ttk.Entry(self.box,name=_name)
         entry.grid(column=_col, row=_row, sticky=tk.E, padx=PADDING_ENTER_X, pady=PADDING_ENTER_Y)
         return entry
     
-    def set_lab_and_enter(self,_text,_row=0,_col=0) -> tuple[tk.Label,ttk.Entry]:
+    def set_lab_and_enter(self,_text,_name="none",_row=0,_col=0) -> tuple[tk.Label,ttk.Entry]:
         next_row = TkinterGui.label_row_count
         if TkinterGui.label_row_count == 0:
             label = self.set_label(_text,_row,_col)
-            enter = self.set_entery(_row,_col+1)
+            enter = self.set_entery(_name,_row,_col+1)
         else:
             label = self.set_label(_text,next_row,_col)
-            enter = self.set_entery(next_row,_col+1)
+            enter = self.set_entery(_name,next_row,_col+1)
             TkinterGui.label_row_count+=1
         return (label,enter)
     
-    def handle_enter(self, event, nr: int):
-        text = self.widgets[nr][1].get()
+    def handle_enter(self, event, para: str):
+        widget: ttk.Entry = self.box.nametowidget(para)
+        text = widget.get()
+        block_id = self.block_id_value["text"]
+        update_block(block_id,para,text)
         # Do something with the entered text
 
-        print("Entered text in nr", nr, ":", text)
+        print("Entered text in ", para, ":", text)
 
     def process_message_queue(self):
         # print("gui queue run")
@@ -106,21 +107,28 @@ class TkinterGui:
             if isinstance(message, dict) and "action" in message:
                 if message["action"] == "update_gui":
                     message.pop("action")
-                    for widget in self.widgets:
-                        widget[1].delete(0, tk.END)
-                        widget[1].grid_forget()
-                        widget[0].grid_forget()
+                    for key in self.widgets:
+                        self.widgets[key][1].unbind('<Return>')
+                        self.widgets[key][1].delete(0, tk.END)
+                        self.widgets[key][1].grid_forget()
+                        self.widgets[key][0].grid_forget()
 
-                    self.block_id.configure(text=f"Block ID: {message.pop('id')}")
-                    self.block_id.grid(column=0,columnspan=2,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
+                    self.block_id.configure(text="Block ID: ")
+                    self.block_id.grid(column=0,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
+                    self.block_id_value.configure(text=message.pop('id'))
+                    self.block_id_value.grid(column=1,row=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y)
 
+                    print("queueue gui: {x}".format(x=message))
                     for idx,mes in enumerate(message):
-                        self.widgets[idx][0].configure(text=f"{mes.capitalize()}: ",justify="left",anchor="w",width=10)
-                        self.widgets[idx][1].insert(0,message[mes])
-                        self.widgets[idx][0].grid(row=idx+1,column=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y,sticky="nsew")
-                        self.widgets[idx][1].grid(row=idx+1,column=1, sticky=tk.E, padx=PADDING_ENTER_X, pady=PADDING_ENTER_Y)
+                        print(f"this is mes: {mes}")
+                        self.widgets[mes][0].configure(text=f"{mes}: ",justify="left",anchor="w",width=10)
+                        self.widgets[mes][1].insert(0,message[mes])
+                        self.widgets[mes][1].widgetName = mes
+                        print(f"name: {self.widgets[mes][1].winfo_name()}")
+                        self.widgets[mes][1].bind('<Return>', lambda event, name=mes: self.handle_enter(event, name))
+                        self.widgets[mes][0].grid(row=idx+1,column=0,padx=PADDING_LABEL_X,pady=PADDING_LABEL_Y,sticky="nsew")
+                        self.widgets[mes][1].grid(row=idx+1,column=1, sticky=tk.E, padx=PADDING_ENTER_X, pady=PADDING_ENTER_Y)
 
-                    print("queueue {x}".format(x=message))
             # Handle other message types if needed
         except queue.Empty:
             pass
@@ -135,13 +143,9 @@ def update_gui(object: Block):
         item[param] = object.params[param]
     GUI_QUEUE.put(item)
     print({object.id})
-    
-# Function to update the variables of the Block object
-def update_block(parameter,new_value):
-    # Update the variables of the Game object here
-    PY_QUEUE.put({"action": "update_block", parameter: new_value})
-    print(f"update: {new_value}")
 
-# def update_block(block_id):
-#     PY_QUEUE.put({"action": "update_block", "parameter": OPTIONS[option]})
-#     print({OPTIONS[option]})
+# Function to update the variables of the Block object
+def update_block(block_id,parameter,new_value):
+    # Update the variables of the Game object here
+    PY_QUEUE.put({"action": "update_block","block": block_id, parameter: new_value})
+    print(f"update_block: {block_id} {parameter} {new_value}")
